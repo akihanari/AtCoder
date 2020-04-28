1c6ba8fac689ef02d31544e83682558c9f1cb510
akihanari <akihanari@gmail.com>
2020-04-29 00:49:28 +0900

    20200428

---
+++ ABC012/A.py
@@ -0,0 +1,6 @@
+# coding: utf-8
+A, B = map(int, input().split())
+T = A
+A = B
+B = T
+print(A, B)
---
+++ ABC013/A.py
@@ -0,0 +1,4 @@
+# coding: utf-8
+X = input()
+lst = ['','A', 'B', 'C', 'D', 'E']
+print(lst.index(X))
